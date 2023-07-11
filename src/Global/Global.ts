c7b69032a52852623c2fa4a44ef0c26d31c9d418
Jorge Munárriz Gutiérrez-Solana <munarrizjorge@gmail.com>
2023-07-11 12:10:40 +0200

    Creating Api fetch

---
+++ src/Global/Global.ts
@@ -0,0 +1,12 @@
+const Global = {
+	playlistLimit: 50,
+	songLimit: 100,
+	access_token: "",
+	refresh_token: "",
+	redirect_uri : "http://localhost:5175/",
+	client_id : "2d3b013718d3445b93173cde5ac34a81",
+	client_secret : "f4bcb1f2b250422986de76485ae61fd5",
+	scopes : "user-read-private user-read-email playlist-read-private"
+}
+
+export default Global;
