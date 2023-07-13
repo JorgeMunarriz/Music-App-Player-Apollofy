65bead5b3a7c680df09c4bcedacdccc536216106
NestorME <menendezechevarria@gmail.com>
2023-07-13 19:47:10 +0200

    verifing branch problems

--- src/components/homeMainContainer/homeMainContainer.styles.ts
+++ src/components/homeMainContainer/homeMainContainer.styles.ts
@@ -9,24 +9,10 @@ export const HomeMainContainerStyles = styled.main`
     
 }
 
-@media (min-device-width: 481px) and (max-device-width: 767px) {
+@media only screen and (min-device-width: 481px) and (max-device-width: 767px) {
   background-color: rgba(0, 0, 0,0.8);
   grid-area: 1 / 1 / 5 / 7;
   
-}
-
-@media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
-  display: grid;
-  grid-template-rows: 1fr 3fr 3fr 3fr 1fr;
-  grid-template-columns: repeat(5, 1fr);
-  height: 100vh;
-}
-
-@media (min-device-width: 481px) and (max-device-width: 767px) {
-  display: grid;
-  grid-template-rows: 1fr 3fr 3fr 3fr 1fr;
-  grid-template-columns: repeat(5, 1fr);
-  height: 100vh;
 }
 
 @media only screen and (min-device-width : 768px) and (max-device-width : 1024px) {
