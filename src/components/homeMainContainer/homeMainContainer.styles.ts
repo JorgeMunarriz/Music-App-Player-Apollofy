d0b8628e33a46959a606a9b562cec71983cfdd91
NestorME <menendezechevarria@gmail.com>
2023-07-13 17:46:20 +0200

    Created ProfilePage and ReproPage structure

--- src/components/homeMainContainer/homeMainContainer.styles.ts
+++ src/components/homeMainContainer/homeMainContainer.styles.ts
@@ -18,17 +18,21 @@ export const HomeMainContainerStyles = styled.main`
 }
 
 @media only screen and (min-device-width : 768px) and (max-device-width : 1024px) {
-  background-color: rgba(0, 0, 0,0.8);
   grid-area: 1 / 2 / 5 / 6;
   width: 100%;
   height: 100%;
+  background-color: rgba(50, 50, 50, 1);
+  border: 1px solid rgba(66, 66, 66, 0.4);
+  border-radius: 0.25rem;
 }
 
 @media only screen and (min-width : 1025px) {
-  background-color: rgba(0, 0, 0,0.8);
   grid-area: 1 / 2 / 5 / 6;
   width: 100%;
   height: 100%;
+  background-color: rgba(50, 50, 50, 1);
+  border: 1px solid rgba(66, 66, 66, 0.4);
+  border-radius: 0.25rem;
 }
 
 `;
