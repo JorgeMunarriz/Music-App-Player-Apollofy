48b454fbb7e9fb2b4b8b7641b6f40dd44c077206
Seo-Moldes <seomoldes@gmail.com>
2023-07-14 11:19:23 +0100

    MusicContext uppdate

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 
 import './App.css'
 import { AuthProvider } from './context/AuthContext'
+import { MusicProvider, useDataTracks } from './context/MusicContext'
 import { Router } from './router/RouterPaths.routes'
 import './styles/Grid.css'
 import './styles/Reset.css'
 
-function App() {
-  
 
+function App() {
+  const {tracks} = useDataTracks()
+console.log(tracks)
   return (
     <>
+    <MusicProvider>
     <AuthProvider name={''} id={0}>
       <Router/>
       </AuthProvider>
+      </MusicProvider>
     </>
   )
 }
