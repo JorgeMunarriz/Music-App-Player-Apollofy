12730db05eedec35c1443539486307e2b3b29751
Jorge Munárriz Gutiérrez-Solana <munarrizjorge@gmail.com>
2023-07-17 11:13:36 +0200

    feat(fetchData) adding Playilist info in to ContainerMain

--- src/api/GetPlaylist.tsx
+++ src/api/GetPlaylist.tsx
@@ -4,10 +4,7 @@ import { urlPlaylist } from '../context'
 
 export const GetPlaylist = async () => {
 
-    const response = await fetch(urlPlaylist ,
-      {
-         "Access-Control-Allow-Origin" : "http://localhost:3000/" 
-      });
+    const response = await fetch(urlPlaylist);
     const data = await response.json();
     console.log(data)
 
--- src/components/sideBar/playlistSection/PlaylistSectionContainer.tsx
+++ src/components/sideBar/playlistSection/PlaylistSectionContainer.tsx
@@ -13,15 +13,13 @@ export const PlaylistSectionContainer = () => {
   return (
     <PlaylistSectionContainerStyles>
         <Link to="" ><i className="bi bi-house-door-fill"></i>Prueba play 1 {}</Link>
-        <Link to="" >Prueba play 2</Link>
-        <Link to="" >Prueba play 3</Link>
         <h2>Playlists:</h2>
-      {playlists?.map((playlist) => (
+      {/* {playlists?.map((playlist) => (
         <div key={playlist.id}>
           <h3>{playlist.name}</h3>
           <p>{playlist.description}</p>
         </div>
-      ))}
+      ))} */}
     </PlaylistSectionContainerStyles>
   );
 };
