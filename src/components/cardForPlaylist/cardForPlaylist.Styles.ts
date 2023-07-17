d879fc7631af30d7a5b7fd8d3a350cefd5e6fd57
NestorME <menendezechevarria@gmail.com>
2023-07-17 15:03:08 +0200

    No changes

--- src/components/cardForPlaylist/cardForPlaylist.Styles.ts
+++ src/components/cardForPlaylist/cardForPlaylist.Styles.ts
@@ -2,20 +2,18 @@ import styled from "styled-components";
 
 export const CardForPlaylistStyles = styled.div`
 img {
-  width: 100px;
-  height: 100px;
+  width: 100%;
+  height: 100%;
 }
  & .card-container {
     display: flex;
     flex-direction: row;
-    max-width: 40%;
-    background: white;
-    width: 300px;
-    height: 50px;
+
+
   }
  & .card-body {
     display: flex;
     flex-direction: column;
-    color: black;
+
   }
 `;
