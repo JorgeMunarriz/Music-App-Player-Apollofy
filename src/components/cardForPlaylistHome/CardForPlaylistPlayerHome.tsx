c8a0190f0a3eeb9368c8fa333c24248fad3bdd49
NestorME <menendezechevarria@gmail.com>
2023-07-19 13:36:01 +0200

    Created toggleLiked function to add or delete tracks or playlists to user favorites DONE and functional for playlists

--- src/components/cardForPlaylistHome/CardForPlaylistPlayerHome.tsx
+++ src/components/cardForPlaylistHome/CardForPlaylistPlayerHome.tsx
@@ -1,13 +1,38 @@
+import { toggleLiked } from "../../api/toggleLiked";
+import { UserContext } from "../../context";
 import { Playlist } from "../../types/data";
 import { CardForPlaylistPlayerHomeStyles } from "./cardForPlaylistPlayerHome.styles";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
-export const CardForPlaylistPlayerHome = ({ thumbnail, name, isFollowed, description }: Playlist) => {
+export const CardForPlaylistPlayerHome = ({id, thumbnail, name, description }: Playlist) => {
+//tengo que traer el puto id
+  const { userLogged, handleUserLogged } = useContext(UserContext);
+  
+  console.log(userLogged)
+
+  const isFollowed = () => {
+    if (userLogged?.favPlaylists.includes(id)) {
+      return true;
+    }
+  return false;
+  }
+
   const [followed, setFollowed] = useState(isFollowed);
 
-  const toggleFollow = () => {
+  const toggleFollow = async () => {
     setFollowed(!followed);
+
+    try {
+      await toggleLiked (userLogged?.id, 'playlist', id, followed ? 'FALSE' : 'TRUE');
+
+      const modifiedUser = { ...userLogged, favPlaylists: followed ? userLogged?.favPlaylists.filter((playlistId) => playlistId !== id) : [...userLogged?.favPlaylists, id] };
+
+      handleUserLogged(modifiedUser);
+
+    } catch (error) {
+      console.error('Error al cambiar la lista favorita:', error);
+    }
   };
 
   return (
@@ -19,7 +44,7 @@ export const CardForPlaylistPlayerHome = ({ thumbnail, name, isFollowed, descrip
         <h3 className="cardForPlaylistPlayer__name">{name}</h3>
         {/* <span className="cardForPlaylistPlayer__description playlist-description">{description}</span> */}
         <button onClick={toggleFollow} className="cardForPlaylistPlayer__follow-btn follow_btn">
-          {followed ? <AiFillHeart size={20} className="full-heart" /> : <AiOutlineHeart size={15} />}
+          {followed ? <AiFillHeart size={20} className="full-heart" /> : <AiOutlineHeart size={10} />}
         </button>
       </div>
     </CardForPlaylistPlayerHomeStyles>
