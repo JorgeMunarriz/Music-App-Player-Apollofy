2902f1dcc005e1ddfc2023b2dd58e2a9baf53553
NestorME <menendezechevarria@gmail.com>
2023-07-20 14:31:37 +0200

    Fitted Searchbar to Standard

--- src/components/mainPlayer/playerMainContainer.styles.ts
+++ src/components/mainPlayer/playerMainContainer.styles.ts
@@ -23,14 +23,14 @@ background: linear-gradient(#340034, #000);
 }
 
 @media only screen and (min-width : 768px) and (max-width : 1024px) {
-  grid-area: 1 / 2 / 6 / 7;
+  grid-area: 1 / 2 / 5 / 7;
   
   width: 100%;
   height: 100%;
 }
 
 @media only screen and (min-width : 1024px) {
-  grid-area: 1 / 2 / 6 / 7;
+  grid-area: 1 / 2 / 5 / 7;
   display: grid;
 
   
