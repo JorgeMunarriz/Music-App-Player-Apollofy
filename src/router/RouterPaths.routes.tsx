3d16e2ada5e6343a3dfc56b493e5bb5990701ade
Facundo Herrera <facundhfed@gmail.com>
2023-09-12 13:08:43 +0200

    Private route fixed

--- src/router/RouterPaths.routes.tsx
+++ src/router/RouterPaths.routes.tsx
@@ -5,7 +5,7 @@ import { LandingPage, LoginPage, RegisterPage, StartingPage } from '../pages/ind
 import '../App.css';
 import RouteSkeletor from '../assets/skeleton/routeSkeletor.tsx';
 import { HOME, LANDING, LOGIN, PLAYER, PLAYERID, PLAYLISTS, PROFILE, REGISTER, STARTING } from '../config/routes/paths.ts';
-import { PrivateRoute, PublicRoute } from '../components/index.ts';
+import { PublicRoute } from '../components/index.ts';
 import { ProtectedRoutes } from '../utils/ProtectedRoutes.tsx';
 import { useAuth0 } from '@auth0/auth0-react'
 
@@ -17,72 +17,73 @@ const LazyPLayerPage = lazy(() => import('../pages/private/PlayerPage.tsx'));
 const LazyPlaylistPage = lazy(() => import('../pages/private/PlaylistPage.tsx'));
 
 export const Router = () => {
-  const { user } = useAuth0()
-  return (
-    <BrowserRouter>
-      {/* <AnimatedRoutes /> */}
-      <Routes>
-        <Route path={LANDING} element={<PublicRoute />}>
-          <Route index element={<LandingPage />} />
-          <Route path={STARTING} element={<StartingPage />} />
-          <Route path={LOGIN} element={<LoginPage />} />
-          <Route path={REGISTER} element={<RegisterPage />} />
-        </Route>
+	const { user } = useAuth0();
+	console.log(user)
+	return (
+		<BrowserRouter>
+			{/* <AnimatedRoutes /> */}
+			<Routes>
+				<Route path={LANDING} element={<PublicRoute />}>
+					<Route index element={<LandingPage />} />
+					<Route path={STARTING} element={<StartingPage />} />
+					<Route path={LOGIN} element={<LoginPage />} />
+					<Route path={REGISTER} element={<RegisterPage />} />
+				</Route>
 
-        <Route path={HOME} element={<Suspense fallback={<RouteSkeletor />}><LazyLayout /></Suspense>}>
+				<Route path={HOME} element={<Suspense fallback={<RouteSkeletor />}><LazyLayout /></Suspense>}>
 
-          <Route index element={<Suspense fallback={<RouteSkeletor />}>
-            <ProtectedRoutes user={user}><LazyHomePage /></ProtectedRoutes>
-          </Suspense>
-          }
-          />
-          <Route path={PLAYER}
-            element={
-              <Suspense fallback={<RouteSkeletor />}>
-                <ProtectedRoutes user={user}><LazyPLayerPage /></ProtectedRoutes>
-              </Suspense>
-            }
-          />
-          <Route path={PLAYERID}
-            element={
-              <Suspense fallback={<RouteSkeletor />}>
-                <ProtectedRoutes user={user}><LazyPLayerPage /></ProtectedRoutes>
-              </Suspense>
-            }
-          />
-          <Route path={PLAYLISTS}
-            element={
-              <Suspense fallback={<RouteSkeletor />}>
-                <ProtectedRoutes user={user}><LazyPlaylistPage /></ProtectedRoutes>
-              </Suspense>
-            }
-          />
-          <Route
-            path={PROFILE}
-            element={
-              <Suspense fallback={<RouteSkeletor />}>
-                <ProtectedRoutes user={user}><LazyProfilePage /></ProtectedRoutes>
-              </Suspense>
-            }
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+					<Route index element={<Suspense fallback={<RouteSkeletor />}>
+						<ProtectedRoutes user={user}><LazyHomePage /></ProtectedRoutes>
+					</Suspense>
+					}
+					/>
+					<Route path={PLAYER}
+						element={
+							<Suspense fallback={<RouteSkeletor />}>
+								<ProtectedRoutes user={user}><LazyPLayerPage /></ProtectedRoutes>
+							</Suspense>
+						}
+					/>
+					<Route path={PLAYERID}
+						element={
+							<Suspense fallback={<RouteSkeletor />}>
+								<ProtectedRoutes user={user}><LazyPLayerPage /></ProtectedRoutes>
+							</Suspense>
+						}
+					/>
+					<Route path={PLAYLISTS}
+						element={
+							<Suspense fallback={<RouteSkeletor />}>
+								<ProtectedRoutes user={user}><LazyPlaylistPage /></ProtectedRoutes>
+							</Suspense>
+						}
+					/>
+					<Route
+						path={PROFILE}
+						element={
+							<Suspense fallback={<RouteSkeletor />}>
+								<ProtectedRoutes user={user}><LazyProfilePage /></ProtectedRoutes>
+							</Suspense>
+						}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 //  pages:
-     // - entrada
-     // - registro
-     // - app
+// - entrada
+// - registro
+// - app
 
 //  header
 //  aside
 //  main
-      // home
-      // player
-      // playlists
-      // profile
+// home
+// player
+// playlists
+// profile
 
 //  footer
 
