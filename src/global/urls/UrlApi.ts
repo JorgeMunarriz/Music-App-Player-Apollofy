5f2667c2a11659c560ca4327b6f5e79beedb5372
Jorge Munárriz Gutiérrez-Solana <munarrizjorge@gmail.com>
2023-09-13 13:49:48 +0200

    request and url created

--- src/global/urls/UrlApi.ts
+++ src/global/urls/UrlApi.ts
@@ -1,7 +1,12 @@
-export const url = "http://localhost:4000";
-export const urlPlaylist = `${url}/playlists`;
-export const urlTracks = `${url}/tracks`;
-export const urlUser = `${url}/user`;
-export const urlAlbums = `${url}/albums`;
-export const urlArtist = `${url}/artists`;
-export const urlGenres = `${url}/genres`;
+export const VITE_API: string = import.meta.env.VITE_API_URL
+
+export const urlPlaylist = `${VITE_API}playlist`;
+export const urlTrack = `${VITE_API}track`;
+export const urlUser = `${VITE_API}user`;
+export const urlAlbum = `${VITE_API}album`;
+export const urlArtis = `${VITE_API}artist`;
+export const urlGenre = `${VITE_API}genre`;
+export const urlPost = `${VITE_API}post`;
+
+
+console.log(urlAlbum)
