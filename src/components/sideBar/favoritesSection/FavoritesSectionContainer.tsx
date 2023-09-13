3e3d1ccfb48443cc0f8bc61afc1edff4a1cdb614
Facundo Herrera <facundhfed@gmail.com>
2023-09-13 12:53:36 +0200

    Inex.ts ando facvoritesectioncontanier fixed

--- src/components/sideBar/favoritesSection/FavoritesSectionContainer.tsx
+++ src/components/sideBar/favoritesSection/FavoritesSectionContainer.tsx
@@ -28,15 +28,4 @@ export const FavoritesSectionContainerStyles = styled.div`
 		color: rgba(255, 255, 255, 1);
 	}
 
-// @media (${breakpoints.min}px <= width <= ${breakpoints.mobileMax}px) 
-		
-// @media (${breakpoints.mobileMax}px < width <= ${breakpoints.tabletMax}px)
-
-// @media (${breakpoints.tabletMax}px < width <= ${breakpoints.laptopsMax}px)
-
-// @media (${breakpoints.laptopsMax}px < width <= ${breakpoints.desktopMax}px)
-
-// @media (width > ${breakpoints.desktopMax}px)
-
-
 `;
