aa06d57e0b5fb71ee142536e258160a7c450629f
Fran Pérez Alberca <fran.perez.alberca@gmail.com>
2023-09-13 13:48:31 +0200

    feat(dev)fix usercontext

--- src/pages/private/HomePage.tsx
+++ src/pages/private/HomePage.tsx
@@ -1,31 +1,24 @@
 
 import { useEffect } from "react";
-import { Footer, HomeMainContainer, SideBar } from "../../components"
+import { HomeMainContainer } from "../../components"
 import { useUserContext } from "../../context";
-import { GlobalPrivatePageStyles } from "../../global"
 import { useAuth0 } from "@auth0/auth0-react";
 
 
 
 const HomePage = () => {
-  const { userFechture } = useUserContext()
-  const { user, getAccessTokenSilently, isAuthenticated } = useAuth0();
+  const { userFetch } = useUserContext()
+  const { user, getAccessTokenSilently} = useAuth0();
 
   console.log(user)
 
   useEffect(() => {
-    userFechture(user, getAccessTokenSilently);
+    userFetch(user, getAccessTokenSilently);
   }, [user]);
 
   return (
 
     <HomeMainContainer />
-
-    // <GlobalPrivatePageStyles>
-    //   <SideBar />
-    //   <Outlet />
-    //   <Footer />
-    // </GlobalPrivatePageStyles>
   )
 }
 
