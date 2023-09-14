0f51954791ab46da08d574b9fd22e3eeaa8090e5
Horacio Martinez <litstyle17@gmail.com>
2023-09-14 21:10:06 +0200

    feat: Add context and request functions for user data
    A context called UserMusicContext was created to manage the user's music-related data, including created playlists, liked playlists, albums, and tracks. Added request functions to get this data and provided access through context.
    
    Changes made:
    - The UserMusicContext context was created.
    - Added statuses and features to get created playlists, liked playlists, albums and tracks.

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import './styles/font.css'
 import { UserProvider } from './context';
 import { GenresProvider } from './context/GenresContext';
 import { TrackProvider } from './context/TrackContext';
+import { UserMusicProvider } from './context/UserMusicContext';
 
 function App() {
 
 	return (
 		<>
 			<UserProvider>
+				<UserMusicProvider>
 				<GenresProvider>
 					<TrackProvider>
 						<Router />
 					</TrackProvider>
 				</GenresProvider>
+				</UserMusicProvider>
 			</UserProvider>
 
 		</>
