bc7bf73841b87ea9a44ef0706d3e2fae757e4232
Fran Pérez Alberca <fran.perez.alberca@gmail.com>
2023-09-21 08:44:39 +0200

    feat(main)change placeholder in searchbar

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -24,7 +24,7 @@ export const SearchBar = ({ setSearchParams, searchParams, handleChangeParams, q
         <SearchBarIcon>
           <BsSearch />
         </SearchBarIcon>
-        <input type="search" name="search" placeholder="Search for Genre" value={query} onChange={handleChangeParams} />
+        <input type="search" name="search" placeholder="Search" value={query} onChange={handleChangeParams} />
       </SearchBarLeft>
       <SearchBarRight>
         {isAuthenticated ? (
